f8aff8cbee1885fbe36428d27ae87b0d19d08bc6
Deborah Savage <savaged725@yahoo.com>
2019-07-16 21:40:20 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,7 @@
-# Add your code here
+def juke_box_lab_sucks
+  {
+  why =>  "all of these key concepts were never really taught"
+  i_am_lost => "and it sucks"
+  now => "im reconsidering if faltirin is right for me since my tests keep passing even tho i have no clue. "
+  }
+end
